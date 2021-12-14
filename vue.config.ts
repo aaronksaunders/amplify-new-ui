9a709288ab50bd3c7d7293db8ba1ba3f94aa1c9d
aaron saunders <>
2021-12-14 16:49:59 -0500

    added vue.config.js

--- vue.config.ts
+++ vue.config.ts
@@ -1,16 +1,17 @@
 // vue.config.js
 module.exports = {
-    // ignore Stencil web components
-    chainWebpack: config => {
-      config.module
-        .rule('vue')
-        .use('vue-loader')
-        .tap(options => {
-          options.compilerOptions = {
-            ...options.compilerOptions,
+  // in webpack config
+  configureWebpack: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+        options: {
+          compilerOptions: {
             isCustomElement: tag => tag.startsWith('amplify-')
           }
-          return options
-        })
-    },
+        }
+      }
+    ]
   }
+}
